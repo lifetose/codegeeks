fd6c129c7ce4501a3262afb5783704103a2bfb77
sashalehedza <sashalehedza@gmail.com>
2025-04-17 16:30:18 +0300

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import configuration from './configs/configuration';
 import { PostgresModule } from './modules/postgres/postgres.module';
 import { RepositoryModule } from './modules/repository/repository.module';
@@ -12,11 +14,16 @@ import { EventsModule } from './modules/events/event.module';
       load: [configuration],
       isGlobal: true,
     }),
-
     RepositoryModule,
     PostgresModule,
 
     EventsModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
   ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,43 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import { SwaggerHelper } from './common/helpers/swagger.helper';
 import { AppConfig } from './configs/config.type';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('March-2024 NestJS')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addBearerAuth({
+      in: 'header',
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .build();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: false,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerHelper.setDefaultResponses(document);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+      defaultModelsExpandDepth: 7,
+      persistAuthorization: true,
+    },
+  });
 
   const configService = app.get(ConfigService);
 
