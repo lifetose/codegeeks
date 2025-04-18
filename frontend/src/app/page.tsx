eb5e144ebaa7edc98127fd687a86bd9fdbc2e32c
sashalehedza <sashalehedza@gmail.com>
2025-04-18 09:11:52 +0300

    fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -26,7 +26,6 @@ export default function EventsPage() {
     async function fetchEvents() {
       const { data } = await getEvents();
       setEvents(data);
-      setFilteredEvents(data);
     }
 
     fetchEvents();
